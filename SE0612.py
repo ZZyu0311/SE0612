7a554f1e49915db7200957cbe7f92469aa5cb667
ZZyu0311 <134933431+ZZyu0311@users.noreply.github.com>
2024-06-12 23:24:38 +0800

    Add files via upload

--- SE0612.py
+++ SE0612.py
@@ -1,13 +1,27 @@
 ﻿import json
 import argparse
 
-class JSONExplorer:
-    def __init__(self, style, icon_family):
-        self.style = style
-        self.icon_family = icon_family
+class IconKu:
+    def getNodeIcon(self):
+        raise NotImplementedError
 
-    def render(self, json_data):
-        return self.style.render(json_data, self.icon_family)
+    def getLeafIcon(self):
+        raise NotImplementedError
+
+class NiceIcon(IconKu):
+    def getNodeIcon(self):
+        return "♢"
+
+    def getLeafIcon(self):
+        return "♤"
+
+class StarIcon(IconKu):
+    def getNodeIcon(self):
+        return "★"
+
+    def getLeafIcon(self):
+        return "☆"
+
 
 class Style:
     def render(self, json_data, icons):
@@ -28,7 +42,7 @@ class TreeStyle(Style):
                 result += f"{indent}└─{icons.getLeafIcon()}{key}: {value}\n"
         return result
 
-class RectStyle(Style):
+class RectangleStyle(Style):
     def render(self, json_data, icons):
         return self._render_node(json_data, 0, icons)
 
@@ -43,60 +57,46 @@ class RectStyle(Style):
                 result += f"{indent}└─{icons.getLeafIcon()}{key}: {value}─" + "─" * 30 + "┤\n"
         return result
 
-class IconFamily:
-    def getNodeIcon(self):
-        raise NotImplementedError
-
-    def getLeafIcon(self):
-        raise NotImplementedError
-
-class PokerIcons(IconFamily):
-    def getNodeIcon(self):
-        return "♢"
-
-    def getLeafIcon(self):
-        return "♤"
-
-class OtherIcons(IconFamily):
-    def getNodeIcon(self):
-        return "★"
-
-    def getLeafIcon(self):
-        return "☆"
-
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Funny JSON Explorer')
     parser.add_argument('-f', '--file', type=str, required=True, help='Path to the JSON file')
-    parser.add_argument('-s', '--style', type=str, required=True, choices=['tree', 'rect'], help='Visualization style (tree or rect)')
-    parser.add_argument('-i', '--icon', type=str, required=True, choices=['poker', 'other'], help='Icon family (poker or other)')
+    parser.add_argument('-s', '--style', type=str, required=True, choices=['tree', 'rectangle'], help='tree or rectangle')
+    parser.add_argument('-i', '--icon', type=str, required=True, choices=['nice', 'star'], help='nice or star')
     return parser.parse_args()
 
-def load_json(file_path):
+def get_jsonfile(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
 def get_style(style_name):
     if style_name == 'tree':
         return TreeStyle()
-    elif style_name == 'rect':
-        return RectStyle()
+    elif style_name == 'rectangle':
+        return RectangleStyle()
     else:
         raise ValueError(f"Unknown style: {style_name}")
 
-def get_icon_family(icon_name):
-    if icon_name == 'poker':
-        return PokerIcons()
-    elif icon_name == 'other':
-        return OtherIcons()
+def get_icon(icon_name):
+    if icon_name == 'nice':
+        return NiceIcon()
+    elif icon_name == 'star':
+        return StarIcon()
     else:
-        raise ValueError(f"Unknown icon family: {icon_name}")
+        raise ValueError(f"Unknown icon: {icon_name}")
 
-# 主程序
+class JSONExplorer:
+    def __init__(self, style, icon_family):
+        self.style = style
+        self.icon_family = icon_family
+
+    def render(self, json_data):
+        return self.style.render(json_data, self.icon_family)
+        
 def main():
     args = parse_arguments()
-    json_data = load_json(args.file)
+    json_data = get_jsonfile(args.file)
     style = get_style(args.style)
-    icon_family = get_icon_family(args.icon)
+    icon_family = get_icon(args.icon)
 
     explorer = JSONExplorer(style, icon_family)
     print(explorer.render(json_data))
